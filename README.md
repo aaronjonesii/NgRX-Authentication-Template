546a39250ccda51a31e9f48b225e510b167a7875
Anonymous One <anonymousone@Anonymous-MB-Pro.local>
2019-03-31 00:45:20 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NgRX-Authentication-Template
-Authentication Angular Application Using NgRx For State Management
+Authentication Angular Application Template Using NgRx For State Management
+
+This Repo is to be used as a starting point for a backend server using JWT authentication. Add custom components to tailor the template to your needs.
---
+++ src/app/shared/auth/state/auth.reducer.ts
@@ -0,0 +1,62 @@
+import { AuthActionTypes, All } from './auth.actions';
+import { User } from '../../models/user';
+
+export interface State {
+  loading: boolean; // Loading?
+  loaded: boolean; // Loaded?
+  isAuthenticated: boolean; // User Logged In?
+  user: User | null; // If authenticated, there should be a user object
+  errorMessage: string | null; // error Message
+}
+
+export const initialState: State = {
+  loading: false,
+  loaded: false,
+  isAuthenticated: false,
+  user: null,
+  errorMessage: null
+};
+
+
+export function reducer(state = initialState, action: All): State {
+  switch (action.type) {
+    case AuthActionTypes.CHECK_AUTHENTICATION: { return { ...state, loading: true}; }
+    // Separator
+    case AuthActionTypes.AUTHENTICATED: { return { ...state, }; }
+    // Separartor
+    case AuthActionTypes.NOT_AUTHENTICATED: { return { ...state, loading: false, loaded: true }; }
+    // Separartor
+    case AuthActionTypes.CHECK_SESSION: { return { ...state, loading: true, loaded: false }; }
+    // Separartor
+    case AuthActionTypes.SESSION_SUCCESS: { return { ...state, loading: false, loaded: true, isAuthenticated: true, errorMessage: null }; }
+    // Separartor
+    case AuthActionTypes.SESSION_EXPIRED: { return { ...state, errorMessage: 'Session Expired, Refreshing now' }; }
+    // Separartor
+    case AuthActionTypes.REFRESH_SESSION: { return { ...state }; }
+    // Separartor
+    case AuthActionTypes.REFRESH_SESSION_SUCCESS: { return { ...state, loading: false, errorMessage: null }; }
+    // Separartor
+    case AuthActionTypes.REFRESH_SESSION_FAILURE: { return { ...state, errorMessage: 'Failed to Refresh Session, check access token.' }; }
+    // Separartor
+    case AuthActionTypes.GET_USER_INFO: { return { ...state, loading: true, loaded: false, }; }
+    // Separartor
+    case AuthActionTypes.GET_USER_INFO_SUCCESS: { return { ...state, loading: false, loaded: true, user: action.payload, errorMessage: null }; }
+    // Separartor
+    case AuthActionTypes.LOGIN: { return { ...state, loading: true, loaded: false }; }
+    // Separartor
+    case AuthActionTypes.LOGIN_SUCCESS: { return { ...state, errorMessage: null }; }
+    // Separator
+    case AuthActionTypes.LOGIN_FAILURE: { return { ...state, errorMessage: 'Incorrect email and/or password.' }; }
+    // Separartor
+    case AuthActionTypes.SIGNUP: { return { ...state, loading: true, loaded: false }; }
+    // Separator
+    case AuthActionTypes.SIGNUP_SUCCESS: { return { ...state, errorMessage: null }; }
+    // Separator
+    case AuthActionTypes.SIGNUP_FAILURE: { return { ...state, errorMessage: 'That username is already in use.' }; }
+    // Separator
+    case AuthActionTypes.LOGOUT: { return initialState; }
+    // Separator
+    default: { return state; }
+  }
+
+}
